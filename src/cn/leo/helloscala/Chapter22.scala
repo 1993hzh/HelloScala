caa77f0aece4b6f5add7cbdac7c67132cd1e5a31
Leo <mail@huzhonghua.cn>
2015-10-13 15:38:32 +0800

    Chapter 22

---
+++ src/cn/leo/helloscala/Chapter22.scala
@@ -0,0 +1,17 @@
+package cn.leo.helloscala
+
+/**
+ * Created by leo on 15-10-13.
+ */
+object Chapter22 extends App {
+  //TODO
+
+
+  //  var cont: (Unit => Unit) = null
+  //  var filename = "myFile.txt"
+  //  var contents = ""
+  //
+  //  reset {
+  //
+  //  }
+}
